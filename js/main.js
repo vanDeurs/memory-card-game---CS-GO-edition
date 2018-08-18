5f2eb9a2d801957125e09fecd74c7e72f75116fb
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-08-18 18:07:56 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const cards = document.querySelectorAll('.card');
+
+let firstCard, secondCard;
+let hasFlippedCard = false;
+let lockBoard = false;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.toggle('flip');
+    
+    if (!hasFlippedCard) {
+        // First card
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // Second card
+        hasFlippedCard = false;
+        secondCard = this;
+
+        checkForMatch();
+    }
+}
+
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.weapon === secondCard.dataset.weapon;
+    isMatch ? disableCards() : unFlipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unFlipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+    }, 1200);
+}
+
+(function shuffleCards() {
+    cards.forEach(card => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    });
+})();
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
